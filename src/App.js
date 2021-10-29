03bbb3377f7dd2a749a329d2e60371cb55166200
Muginga <74222775+muginga72@users.noreply.github.com>
2021-10-28 20:17:32 -0400

    Added the useState and handlers to submit new todo.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './component/AddTodoForm';
 
-
-
+const todoList = [
+  {
+    id: "0",
+    title: "Complete assignment Lesson 1-3",
+  },
+  {
+    id: "1",
+    title: "Upload it to GitHub",
+  },
+  {
+    id: "2",
+    title: "Submit the assignment on Squibby",
+  },
+];
 
 function App() {
+  const [newTodo, setNewTodo] = React.useState('');
+  const [items, setItems] = useState(todoList);
+
+  const addNewTodo = (thing) => {
+    const existing = [...items];
+    existing.unshift({
+      id: items.length,
+      title: thing
+    });
+    setItems(existing);
+    setNewTodo('');
+  };
+  
   return (
     <div>
-      <h1>Todo List</h1>
+      <h1 style={{color: "blue"}}>Todo List</h1>
 
-      {/* Rendering todo list and the form */}
-      <AddTodoForm />
-      <TodoList />
+      <AddTodoForm onAddTodo={addNewTodo} newTodo={newTodo} />
+      <p>{newTodo}</p>
+      <TodoList items={items} />
       
-
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,30 +1,14 @@
-import React from 'react';
-import TodoListItem from './component/TodoListItem';
-
-const todoList = [
-  {
-    id: '0',
-    title: 'Complete assignment Lesson 1-3'
-  },
-  {
-    id: '1',
-    title: 'Upload it to GitHub'
-  },
-  {
-    id: '2',
-    title: 'Submit it to Squibby'
-  },
-];
+import React from "react";
+import TodoListItem from "./component/TodoListItem";
 
 function TodoList(props) {
   return (
     <div>
-      <hr />
-      {todoList.map(function (todo) {
-        return (
-          <TodoListItem key={ todo.id } todo={ todo }/>
-        );
-      })}
+      <div>
+        {props.items.map(function (todo) {
+          return <TodoListItem key={todo.id} todo={todo} />;
+        })}
+      </div>
     </div>
   );
 }
