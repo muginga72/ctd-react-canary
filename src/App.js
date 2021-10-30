1a716cfdfecbba5887d4379286b5db0bec2b65d7
Muginga <74222775+muginga72@users.noreply.github.com>
2021-10-29 21:16:50 -0400

    Added React props, and State, and Handler function and Callback handlers.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './component/AddTodoForm';
 
-const todoList = [
-  {
-    id: "0",
-    title: "Complete assignment Lesson 1-3",
-  },
-  {
-    id: "1",
-    title: "Upload it to GitHub",
-  },
-  {
-    id: "2",
-    title: "Submit the assignment on Squibby",
-  },
-];
-
 function App() {
-  const [newTodo, setNewTodo] = React.useState('');
-  const [items, setItems] = useState(todoList);
-
-  const addNewTodo = (thing) => {
-    const existing = [...items];
-    existing.unshift({
-      id: items.length,
-      title: thing
-    });
-    setItems(existing);
-    setNewTodo('');
-  };
+  const [newTodo, setNewTodo] = useState('');
   
   return (
     <div>
       <h1 style={{color: "blue"}}>Todo List</h1>
 
-      <AddTodoForm onAddTodo={addNewTodo} newTodo={newTodo} />
+      <AddTodoForm onAddTodo={setNewTodo} newTodo={newTodo} />
       <p>{newTodo}</p>
-      <TodoList items={items} />
+      <TodoList />
       
     </div>
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,29 @@
 import React from "react";
 import TodoListItem from "./component/TodoListItem";
 
+const todoList = [
+  {
+    id: "0",
+    title: "Complete assignment Lesson 1-3",
+  },
+  {
+    id: "1",
+    title: "Upload it to GitHub",
+  },
+  {
+    id: "2",
+    title: "Submit the assignment on Squibby",
+  },
+];
+
 function TodoList(props) {
   return (
     <div>
-      <div>
-        {props.items.map(function (todo) {
-          return <TodoListItem key={todo.id} todo={todo} />;
-        })}
-      </div>
+      <ul>
+        {todoList.map(item => (
+          <TodoListItem key={item.id} todo={item} />
+        ))}
+      </ul>
     </div>
   );
 }
